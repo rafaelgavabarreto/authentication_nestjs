ebfaff04d93fd90bb8f6d3d786a1e78862d0874d
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-12 00:03:49 -0400

    more prisma

---
+++ prisma/migrations/20230912034329_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
