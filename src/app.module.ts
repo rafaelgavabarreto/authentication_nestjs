6ceee032382701bca8e008b923c209e5ecfd5884
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-11 21:35:29 -0400

    authentication resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthenticationModule],
   controllers: [AppController],
   providers: [AppService],
 })
