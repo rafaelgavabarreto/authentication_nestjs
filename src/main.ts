ceaa154bad386b1e0f945f8127086db5d842fb0f
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-12 19:04:14 -0400

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication example')
+    .setDescription('The authentication API description')
+    .setVersion('1.0')
+    .addTag('authentication')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
