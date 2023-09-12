8e45b458cc09d8fe78266dd30692cacfdd38e9c9
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-11 23:34:30 -0400

    env and basic prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
