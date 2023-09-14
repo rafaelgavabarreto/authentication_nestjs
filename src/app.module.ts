bce5d38afd9d82c0c8b0b0c5a08ed6187eab6703
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-14 09:59:15 -0400

    throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SystemConfigModule } from './config/systemConfig.module';
 import { routesModules } from './modules/routes.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [SystemConfigModule, routesModules],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    SystemConfigModule,
+    routesModules,
+  ],
 })
 export class AppModule {}
